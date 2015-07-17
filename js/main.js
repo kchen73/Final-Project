2f947ab1f46e2bbb47b898cc094cd20d029c26f1
bilalsddq <bilalsddq@hotmail.com>
2015-07-17 02:25:51 -0700

    created a basic template of the main js, and getting the data from url

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+var app = angular.module('myApp', [])
 
+app.controller('myCtrl', function($scope, $http) {
+	$http.get('URL')
+		.success(function(response) {
+				$scope.data = response.data
+		})
+});
+
+var data;
+$.ajax({
+	url: '/student/{version}/course/{year},{quarter},{curriculum_abbreviation},{course_number}/{section_id}.{xml/json/xhtml}'
+	type: "get"
+	success:function(dat) {
+		data = dat
+		// do something
+	},
+	dataType: "json"
+})
